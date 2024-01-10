d9618b98df405173d4a4966c430beacb706469a7
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-01-10 13:17:35 +0530

    add the create edge method in graph

--- whalegrad/engine/base.py
+++ whalegrad/engine/base.py
@@ -65,12 +65,12 @@ class Node:
      
      return all(parent.visited for parent in self.parents)
      
-  def add_child(self, other):
+  def create_child(self, other):
      
      if other not in self.children:
         self.children.append(other)
 
-  def add_parent(self, other):
+  def create_parent(self, other):
 
      if other not in self.parents:
         self.parents.aappend(other)     
@@ -89,3 +89,43 @@ class Node:
 
  class Graph:
     
+    graph = None # if not provided grpah it will call the global graph
+
+    def __init__(self):
+       
+       self.nodes_dict = {}
+       self.trace = True
+
+    def create_edge(self, final_node, input_nodes):
+       
+       self.create_node(final_node)
+       for i in input_nodes: #i stands for input_node
+           if self.get_node(i) is None:
+              self.create_value(i)   
+           input_node = self.get_node(i)
+           final_node.create_parent(input_node)
+           input_node.create_child(final_node)
+          
+
+          
+    # def create_node(self, node):
+       
+    #    self.nodes_dict[node.vals] = node
+
+
+    # def get_node(self, vals):
+       
+    #    return self.nodes_dict.get(vals)
+    
+    # def create_value(self, vals):
+       
+    #    self.nodes_dict[vals] = Node(vals)
+
+    # def remove_value(self, vals):
+       
+    #    self.nodes_dict.pop(vals)
+
+    # def default_vis(self):
+    #    for node in self.nodes_dict.values():
+    #       node.visited = False
+       
