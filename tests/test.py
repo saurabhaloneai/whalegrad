5dd2160ec9fa34633d93452c5fe6ebc8cf4be014
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-01-14 19:17:55 +0530

    addib new features

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+## make the classifier for binary clssification
+
+# TODO :
+# 1. linear
+# 2. optim => adam , sgd
+# 3. loss => mse, Binary Cross Entropy
+# 4. acc => manual
+# 5. activation => sigmoid, relu and tanh
+# 6. no grad caln for inference like pytoch 
+
