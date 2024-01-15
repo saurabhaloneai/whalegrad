2e64dd25f4ba9d526c4d6d1c57aa798dade9fb89
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-01-15 23:27:14 +0530

    resolve the error rookie mistake

--- tests/test.py
+++ tests/test.py
@@ -15,7 +15,7 @@
 # import numpy as np
 # from whalegrad.nn.layers.base import Module
 
-from whalegrad.nn.acc import acc
+from whalegrad.nn.acc import Accuracy
 
 # linear_layer = Linear(in_features=3, out_features=2)
 
@@ -32,8 +32,17 @@ from whalegrad.nn.acc import acc
 
 import numpy as np
 
-y_preds = np.array([0.9, 0.2, 0.1, 0.5, 0.8 ])
-y_true = np.array([0.9, 0.2, 0.1, 0.5, 0.7 ])
+# y_preds = np.array([0.9, 0.2, 0.1, 0.5, 0.8 ])
+# y_true = np.array([0.9, 0.2, 0.1, 0.5, 0.7 ])
 
-acc = acc(y_preds, y_true)
-print(acc)
+# accc = acc()
+
+# print(accc.forward(y_preds=y_preds, y_true=y_true))
+# Create an instance of the Accuracy class
+accuracy_calculator = Accuracy(y_preds=np.array([1, 20, 90, 89, 50]), y_true=np.array([1, 2, 3, 4, 0]))
+
+# Calculate the accuracy using the forward method
+accuracy = accuracy_calculator.forward()
+
+# Print the result
+print("Accuracy:", accuracy)
