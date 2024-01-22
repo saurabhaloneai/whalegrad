dc38911f6f1c68c58a0e63e2eca6689c3456db76
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-01-22 08:58:18 +0530

    add the sinusoidal positional encoding

--- whalegrad/nn/layers/positional_encoding.py
+++ whalegrad/nn/layers/positional_encoding.py
@@ -78,3 +78,97 @@ class RoPE:
         freqs = np.exp(-np.arange(0.0, D, dtype=dtype) * (np.log(base) / D))
         theta = np.reshape(positions, (-1, 1)) * np.reshape(freqs, (1, -1))
         return np.cos(theta), np.sin(theta)
+
+
+#-----------------------------#
+
+
+class SinusoidalPositionalEncoding:
+    """Implements sinusoidal positional encoding similar to [1].
+
+    [1]: https://arxiv.org/abs/1706.03762
+
+    Args:
+        dims (int): The dimensionality of the resulting positional embeddings.
+        min_freq (float): The minimum frequency expected (default: 0.0001)
+        max_freq (float): The maximum frequency expected (default: 1)
+        scale (float): Scale the embeddings by that number (default: sqrt(dims//2))
+        cos_first (bool): If set to True embed using ``[cos(x); sin(x)]``
+            instead of the other way around (default: False)
+        full_turns (bool): If set to True multiply the frequencies
+            with ``2 pi`` (default: False)
+    """
+
+    def __init__(
+        self,
+        dims: int,
+        min_freq: float = 0.0001,
+        max_freq: float = 1,
+        scale: Optional[float] = None,
+        cos_first: bool = False,
+        full_turns: bool = False,
+    ):
+        one_zero = 1 - np.arange(0, dims // 2) / (dims // 2 - 1)
+        min_freq = np.log(min_freq)
+        max_freq = np.log(max_freq)
+
+        # Start with underscore so it is not included in the parameters
+        self._sigmas = np.exp(one_zero * (max_freq - min_freq) + min_freq)
+        if full_turns:
+            self._sigmas = self._sigmas * (2 * np.pi)
+
+        # Save some constants that define the implementation
+        self.scale = scale or (2 / dims) ** 0.5
+        self.cos_first = cos_first
+
+    def __call__(self, x):
+        y = x[..., None] * self._sigmas
+        cosy = np.cos(y)
+        siny = np.sin(y)
+
+        if self.cos_first:
+            y = np.concatenate([cosy, siny], axis=-1)
+        else:
+            y = np.concatenate([siny, cosy], axis=-1)
+
+        if self.scale != 1:
+            y = y * self.scale
+
+        return y
+
+
+class ALiBi:
+    @staticmethod
+    def create_alibi_matrix(
+        q_sequence_length: int,
+        k_sequence_length: int,
+        num_heads: int,
+        offset: int,
+        dtype=np.float32,
+    ):
+        x1 = np.arange(offset, q_sequence_length)
+        x2 = np.arange(0, k_sequence_length)
+        distance_matrix = -np.abs(
+            np.expand_dims(x1[:, None] - x2[None, :], axis=(0, 1))
+        )
+        alibi_slope = ALiBi.create_alibi_slope(num_heads=num_heads)
+        alibi_mask = (distance_matrix * alibi_slope).astype(dtype)
+        return alibi_mask
+
+    @staticmethod
+    def create_alibi_slope(num_heads):
+        x = (2**8) ** (1 / num_heads)
+        out = np.power(x, -np.arange(1, num_heads + 1))
+        return np.expand_dims(out, axis=(-1, -2))
+
+    def __call__(self, attention_scores, offset=0, mask=None):
+        alibi_mask = ALiBi.create_alibi_matrix(
+            q_sequence_length=attention_scores.shape[-2] + offset,
+            k_sequence_length=attention_scores.shape[-1],
+            num_heads=attention_scores.shape[1],
+            offset=offset,
+            dtype=attention_scores.dtype,
+        )
+        if mask is not None:
+            alibi_mask = alibi_mask + mask
+        return attention_scores + alibi_mask
